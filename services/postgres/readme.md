84e9f4cdf1e96e353b65b62779f59cda9b32797e
jalkanen <jalkanen@student.tut.fi>
2018-10-21 03:50:09 +0300

    parse diets and mock data

--- services/postgres/readme.md
+++ services/postgres/readme.md
@@ -3,6 +3,17 @@ Docker creates database "sipuliton" automatically
 Default schema used is "public"
 
 #About Populating DB
--work in progress
-Quotation policy for csv data files is minimal (=only field with delimiter, quotechar or part(s) of lineterminator are quoted)
-populate_parser.py coverts csv files from data and mock_data into sql files
+Running populate_parser.py in this folder coverts csv files from data and mock_data into sql files
+
+-Format of CSV files
+CSV files use single comma(,) as separator and does not parse any remaining whitespace
+Quotation policy for csv data files is minimal (only fields with delimiter, quotechar or part(s) of line terminator are quoted)
+For diets and groups, first column is reserved for contained other groups separated by semicolons(;)
+
+--Mock data format
+CSV files in mock_data should be of following format:
+file name should be of following format: XX_TABLE.csv
+XX can be a number between 00 and 99 and defines the read order of files
+TABLE is the name of sql table to be populated
+1st line: either empty or table field names
+2nd line and forward: data in same order as defined in 1st row or in sql tables if 1st row is empty
--- services/postgres/sql/03_init_diet.sql
+++ services/postgres/sql/03_init_diet.sql
@@ -1,17 +1,5 @@
 --create diet related tables
 
---food and its name in some language
-CREATE TABLE food(
-    food_id bigint PRIMARY KEY
-);
-
-CREATE TABLE food_name(
-    food_id bigint REFERENCES food,
-    language_id int REFERENCES languages,
-    name varchar(30) NOT NULL,
-    PRIMARY KEY (food_id, language_id)
-);
-
 --food group and its name in some language
 CREATE TABLE food_group(
     food_group_id bigint PRIMARY KEY
@@ -20,15 +8,8 @@ CREATE TABLE food_group(
 CREATE TABLE food_group_name(
     food_group_id bigint REFERENCES food_group,
     language_id int REFERENCES languages,
-    name varchar(30) NOT NULL,
+    name varchar(40) NOT NULL,
     PRIMARY KEY (food_group_id, language_id)
-);
-
---link foods to groups
-CREATE TABLE food_group_foods(
-    food_group_id bigint REFERENCES food_group,
-    food_id bigint REFERENCES food,
-    PRIMARY KEY (food_group_id, food_id)
 );
 
 --link group to groups (make subgroups possible)
@@ -47,7 +28,7 @@ CREATE TABLE global_diet(
 CREATE TABLE global_diet_name(
     global_diet_id bigint REFERENCES global_diet,
     language_id int REFERENCES languages,
-    name varchar(30) NOT NULL,
+    name varchar(40) NOT NULL,
     PRIMARY KEY (global_diet_id, language_id)
 );
 
@@ -58,18 +39,11 @@ CREATE TABLE diet_groups(
     PRIMARY KEY (global_diet_id, food_group_id)
 );
 
---link singular foods to diets
-CREATE TABLE diet_foods(
-    global_diet_id bigint REFERENCES global_diet,
-    food_id bigint REFERENCES food,
-    PRIMARY KEY (global_diet_id, food_id)
-);
-
 --diets for user
 CREATE TABLE diet_name(
     user_id bigint REFERENCES user_profile,
     diet_id int,
     global_diet_id bigint NOT NULL REFERENCES global_diet,
-    name varchar(30) NOT NULL,
+    name varchar(40) NOT NULL,
     PRIMARY KEY (user_id, diet_id)
 );
