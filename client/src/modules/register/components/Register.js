f973325ff48de79ad87fe16b86939e6b1dd94623
275511 <juha.kautto@student.tut.fi>
2018-10-23 19:26:27 +0300

    12 Register - Started working with Cognito.

--- client/src/modules/register/components/Register.js
+++ client/src/modules/register/components/Register.js
@@ -1,7 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {
+  NavItem,
+  NavLink,
+  form,
+  Button
+} from 'reactstrap';
+
+import { Auth } from "aws-amplify";
 class Register extends React.Component
 {
+	
 	handleRegistration(event)
 	{
+		/* do the registration, maybe send the data in other function */
 		console.log('Registration sent');
 	}
 	render()
@@ -11,16 +23,16 @@ class Register extends React.Component
 		<form onSubmit={this.handleRegistration}>
 		<label>
 		Käyttäjätunnus:<br>
-		<input type="text" name="username"/><br>
+		<input type="text" name="username" value={this.state.username} onChange={this.changeUsername}/><br>
 		Salasana:<br>
-		<input type="password" name="pass"/><br>
+		<input type="password" name="password" value={this.state.password} onChange={this.changePass}/><br>
 		Salasana uudelleen:<br>
-		<input type="password" name="retypepass"/><br>
+		<input type="password" name="retypepass" value={this.state.retypePass} onChange={this.changeRetypePass}/><br>
 		Sähköposti:<br>
-		<input type="email" name="mail"/><br>
+		<input type="email" name="mail" value={this.state.mail} onChange={this.changeMail}/><br>
 		Sähköposti uudelleen:<br>
-		<input type="email" name="retypemail"/><br>
-		<input type="radio" name="ula"/>Hyväksyn käyttäjäehdot<br>
+		<input type="email" name="retypemail" value={this.state.retypeMail} onChange={this.changeRetypeMail}/><br>
+		<input type="radio" name="ula" value={this.state.ula} onChange={this.changeUla}/>Hyväksyn käyttäjäehdot<br>
 		</label>
 		<input type="submit"/>
 		</form>
