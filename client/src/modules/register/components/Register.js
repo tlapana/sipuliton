5d7802ed1966df2551eb293fb0bc5a332ce02c5f
275511 <juha.kautto@student.tut.fi>
2018-10-24 23:09:17 +0300

    12 Register - Added a function to check fields and a constructor for defaults.

--- client/src/modules/register/components/Register.js
+++ client/src/modules/register/components/Register.js
@@ -10,11 +10,39 @@ import {
 import { Auth } from "aws-amplify";
 class Register extends React.Component
 {
-	
+	constructor(props)/*construct the default state on first page load*/
+	{
+		super(props);
+
+		this.state = {
+		isLoading: false,
+		username: "",
+		mail: "",
+		password: "",
+		retypeMail: "",
+		retypePass: "",
+		newUser: null
+		};
+	}
 	handleRegistration(event)
 	{
 		/* do the registration, maybe send the data in other function */
 		console.log('Registration sent');
+	}
+	validateForm()/*Make sure all fields are okay*/
+	{
+		return
+		(
+		this.state.username.length > 0 &&
+		this.state.mail.length > 0 &&
+		this.state.password.length > 6 &&
+		this.state.username.length < 50 &&
+		this.state.mail.length < 50 &&
+		this.state.password.length < 30 &&
+		this.state.mail === this.state.retypeMail
+		this.state.password === this.state.retypePass
+		this.state.ula.checked
+		);
 	}
 	render()
 	{
@@ -23,16 +51,16 @@ class Register extends React.Component
 		<form onSubmit={this.handleRegistration}>
 		<label>
 		Käyttäjätunnus:<br>
-		<input type="text" name="username" value={this.state.username} onChange={this.changeUsername}/><br>
+		<input type="text" name="username" value={this.state.username} onChange={this.changeUsername} required/><br>
 		Salasana:<br>
-		<input type="password" name="password" value={this.state.password} onChange={this.changePass}/><br>
+		<input type="password" name="password" value={this.state.password} onChange={this.changePass} required/><br>
 		Salasana uudelleen:<br>
-		<input type="password" name="retypepass" value={this.state.retypePass} onChange={this.changeRetypePass}/><br>
+		<input type="password" name="retypepass" value={this.state.retypePass} onChange={this.changeRetypePass} required/><br>
 		Sähköposti:<br>
-		<input type="email" name="mail" value={this.state.mail} onChange={this.changeMail}/><br>
+		<input type="email" name="mail" value={this.state.mail} onChange={this.changeMail} required/><br>
 		Sähköposti uudelleen:<br>
-		<input type="email" name="retypemail" value={this.state.retypeMail} onChange={this.changeRetypeMail}/><br>
-		<input type="radio" name="ula" value={this.state.ula} onChange={this.changeUla}/>Hyväksyn käyttäjäehdot<br>
+		<input type="email" name="retypemail" value={this.state.retypeMail} onChange={this.changeRetypeMail} required/><br>
+		<input type="checkbox" name="ula" value={this.state.ula} onChange={this.changeUla} required/>Hyväksyn käyttäjäehdot<br>
 		</label>
 		<input type="submit"/>
 		</form>
