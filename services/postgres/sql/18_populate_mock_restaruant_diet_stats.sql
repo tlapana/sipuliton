c83f6121f947766200cfbd557469e60f2417cc03
Sergei Häyrynen <sergei.hayrynen@veracell.eu>
2018-10-28 01:04:24 +0300

    Add mock restaurants and restaurant diet stats

---
+++ services/postgres/sql/18_populate_mock_restaruant_diet_stats.sql
@@ -0,0 +1,3 @@
+INSERT INTO restaurant_diet_stats VALUES(1,0,0,4,3,4,3,3,40);
+INSERT INTO restaurant_diet_stats VALUES(2,0,0,5,5,5,5,5,50);
+INSERT INTO restaurant_diet_stats VALUES(3,0,0,1,1,1,1,1,60);
