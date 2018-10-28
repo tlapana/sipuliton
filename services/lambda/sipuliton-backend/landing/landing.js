a0073998e63c33c5d8f21460b3be0ce04094e20e
Sergei Häyrynen <sergei.hayrynen@veracell.eu>
2018-10-28 21:40:24 +0200

    Restaurant query works now

--- services/lambda/sipuliton-backend/landing/landing.js
+++ services/lambda/sipuliton-backend/landing/landing.js
@@ -37,9 +37,9 @@ let response;
  */
 exports.lambdaHandler = async (event, context) => {
     try {
-        console.log("TRYING")
         // Result of this query will later go to the returned json
         //TODO: fix reliability typo in database
+        //TODO: take diet and location as parameters
         var collectLandingPage = `
         SELECT restaurant.restaurant_id as restaurant_id, name, email, website, street_address, geo_location, 
                rating_overall,
@@ -48,55 +48,44 @@ exports.lambdaHandler = async (event, context) => {
                rating_service_and_quality 
         FROM restaurant INNER JOIN restaurant_diet_stats
         ON restaurant.restaurant_id=restaurant_diet_stats.restaurant_id
-        ORDER BY rating_overall
+        ORDER BY rating_overall DESC
         LIMIT 10;
         `;
         var pg = require("pg");
-        var conn = "postgres://sipuliton:sipuliton@sipulitonpostgres_postgres_1:5432/sipuliton";
-        var client = new pg.Client(conn);
-        console.log("Client created");
-        console.log(client);
-        const result = await client.connect((err) => {
-            console.log("Connecting")
-            if (err){
-                console.log("Failed to connect client")
-                console.log(err)
-            }
-            console.log("Running query")
-            client.query(collectLandingPage, [], (err, res) => {
-                if(err){
-                    console.log('error running query', err);
-                }
-                console.log("Parsing result");
-                var jsonString = JSON.stringify(res.rows);
-                console.log(jsonString);
-                var jsonObj = JSON.parse(jsonString);
-                console.log("Building response")
-                response = {
-                    'statusCode': 200,
-                    //TODO: Handle CORS in AWS api gateway settings prior to deployment
-                    'headers': {
-                        'Access-Control-Allow-Origin': '*'
-                    },
-                    'body': '{"jsonString": "test"}'
-                };
-                console.log(response)
 
-            });
-            console.log("Query done");
-            console.log(response);
-            
+        //TODO: Before deploying, change to a method for fetching Amazon RDS credentials
+        var conn = "postgres://sipuliton:sipuliton@sipulitonpostgres_postgres_1/sipuliton";
+        const client = new pg.Client(conn);
+        await client.connect((err) => {
+                console.log("Connecting")
+                if (err){
+                    console.error("Failed to connect client")
+                    console.error(err)
+                    throw err
+                }
         });
-        await client.end();
-        console.log("Postgres part done")
         
+        const res = await client.query(collectLandingPage);
+        var jsonString = JSON.stringify(res.rows);
+        var jsonObj = JSON.parse(jsonString);
+        var bodyJSON = {'restaurants': jsonObj}
+        await client.end()
+
+        response = {
+            'statusCode': 200,
+            //TODO: Handle CORS in AWS api gateway settings prior to deployment
+            'headers': {
+                'Access-Control-Allow-Origin': '*'
+            },
+            'body': JSON.stringify(bodyJSON)
+        };
+
 
     } catch (err) {
-        console.log("Catched!")
-        console.log(err);
+        console.error(err);
         return err;
     }
-    console.log("Returning response")
+
     console.log(response)
     return response
 };
