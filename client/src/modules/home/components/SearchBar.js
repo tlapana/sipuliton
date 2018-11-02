9931379670a886d41028f9c6f876ad56a8ee08fe
Tommi Mansikka <mansikk6@student.tut.fi>
2018-11-02 18:54:08 +0200

    Installed react-select, made basic test selection

--- client/src/modules/home/components/SearchBar.js
+++ client/src/modules/home/components/SearchBar.js
@@ -6,6 +6,7 @@
 
 import React from 'react';
 import { Button, UncontrolledTooltip, Popover, PopoverBody, PopoverHeader } from 'reactstrap';
+import Select from 'react-select';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from '../../../styles/landingpage.css';
 
@@ -14,11 +15,16 @@ class SearchBar extends React.Component {
   constructor(props, context) {
     super(props, context);
 
+    //Bind the functions
+    this.togglePopover = this.togglePopover.bind(this);
     this.doSearch = this.doSearch.bind(this);
-    this.toggle = this.toggle.bind(this);
+    this.getDefaultValues = this.getDefaultValues.bind(this);
+    this.getOptions = this.getOptions.bind(this);
     
     this.state = {
-      isLoading: false,
+      isLoading: true,
+      loadedDefaults: false,
+      loadedOptions: false,
       popoverOpen: false,
       filters : [],
       keywords : 'placeholder, another place holder, test,    lot of empty space      , double  space  between  words'
@@ -26,7 +32,7 @@ class SearchBar extends React.Component {
   }
   
   //Toggles popover
-  toggle() {
+  togglePopover() {
     this.setState({
       popoverOpen: !this.state.popoverOpen
     });
@@ -51,30 +57,88 @@ class SearchBar extends React.Component {
       console.log(searchTerms);
   }
   
+  
+  //Get the default selections. Mainly checks if user is logged in if is, then get the data
+  getDefaultValues() {
+    
+    const defaultValues = [];
+    this.state.loadedDefaults = true;
+    return defaultValues;
+  }
+  
+  //This gets the options for the selection
+  getOptions() {
+    
+    
+    
+    const options = [
+      { value: 'onions', label: 'Allergia/Sipuli' },
+      { value: 'tomato', label: 'Allergia/Tomaatti' },
+      { value: 'nuts', label: 'Allergia/Pähkinä' },
+      { value: 'lactose', label: 'Laktoositon ruokavalio' },
+      { value: 'coeliac ', label: 'Keliakia' }
+    ]
+    console.log("Getting the options: ")
+    console.log(options);
+    
+    this.state.loadedOptions = true;
+    
+    return options;
+  }
+  
+  renderFilterButton()
+  {
+    if( this.state.loadedDefaults && this.state.loadedOptions) {
+      return (
+        <button type="submit" className="searchBtn" onClick={this.doSearch}>
+          <FontAwesomeIcon icon="search" />
+        </button>
+      );
+    }    
+    else {
+      return (
+        "Ladataan tietoja..."
+      );
+    }
+    
+  }
+  
+  
+  
   render() {
     
     
     return (
       <div className="searchDiv">
         <form id="search-form" class="search" onSubmit={this.login}>
+        
           <input type="text" name="search" className="round" placeholder="Hae kaupungista tai osoitteesta..." />
-          <button type="submit" className="searchBtn" onClick={this.doSearch}>
-              <FontAwesomeIcon icon="search" />
-          </button>
+          
+          
           <span className="instructions" id="instructions-symbol"> ??? </span>  
            <UncontrolledTooltip placement="right" target="instructions-symbol">
             Käytä pilkkua ( , ) erottimena
           </UncontrolledTooltip>
-            
-          <br />
+          
           <br />
           
-          <button className="filterBtn" id="filter_popover" onClick={this.toggle} type="button" >Rajaa</button>
+          <button className="filterBtn" id="filter_popover" onClick={this.togglePopover} type="button" >Rajaa</button>     
           
-          <Popover placement="bottom" isOpen={this.state.popoverOpen} target="filter_popover" toggle={this.toggle}>
-            <PopoverHeader>Popover Title</PopoverHeader>
-            <PopoverBody>Sed posuere consectetur est at lobortis. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</PopoverBody>
+          <Popover placement="bottom" isOpen={this.state.popoverOpen} target="filter_popover" toggle={this.togglePopover}>
+            <PopoverHeader></PopoverHeader>
+            <PopoverBody>
+              Sisällytä hakuun: <br />
+              <Select
+                defaultValue={ this.getDefaultValues() }
+                isMulti
+                name="colors"
+                options={ this.getOptions() }
+                className="basic-multi-select"
+                classNamePrefix="select"
+              />
+            </PopoverBody>
           </Popover>
+          
         </form>
       </div>  
     );
