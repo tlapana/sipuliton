bfe1fe712f3db74d203b38a5180fe53132c677b9
275511 <juha.kautto@student.tut.fi>
2018-11-03 16:18:25 +0200

    Started the restaurant view.

---
+++ client/src/modules/restaurant/components/Restaurant.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { render } from 'react-dom';
+import {
+	Button,
+	Form,
+	FormGroup,
+	Input,
+	Label,
+} from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export default class Restaurant extends React.Component {
+	
+}
