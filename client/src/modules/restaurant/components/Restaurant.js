7460ee64ddb19d05b3ad9cea820206e79be66eb9
275511 <juha.kautto@student.tut.fi>
2018-11-05 15:53:33 +0200

    Render function.

--- client/src/modules/restaurant/components/Restaurant.js
+++ client/src/modules/restaurant/components/Restaurant.js
@@ -14,10 +14,15 @@ export default class Restaurant extends React.Component {
 	constructor(props) {
 		super(props);
 		this.restaurant.name = "Test";
+		this.restaurant.picture = "";
 	}
 	render() {
 		<div id="restaurant">
-		<h2>{this.restaurant.name()}</h2>
+		<h2>{this.restaurant.name()}</h2><br>
+		<div id="restaurantPictures"><img src={this.restaurant.picture()} alt="Restaurant picture"></div>
+		<div id="restaurantStats"></div><br>
+		<div id="restaurantDesc"></div><br>
+		<Input type="button" value="Lisää arvostelu" className="btn btn-primary mb-2">Lisää arvostelu></Input>
 		</div>
 	}
 }
