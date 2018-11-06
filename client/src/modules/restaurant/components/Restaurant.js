eecd5841b204254d148f32cf5e7f0048849c180d
275511 <juha.kautto@student.tut.fi>
2018-11-06 15:46:46 +0200

    Constructor and render functions.

--- client/src/modules/restaurant/components/Restaurant.js
+++ client/src/modules/restaurant/components/Restaurant.js
@@ -14,14 +14,24 @@ export default class Restaurant extends React.Component {
 	constructor(props) {
 		super(props);
 		this.restaurant.name = "Test";
-		this.restaurant.picture = "";
+		this.restaurant.pictures = ["", "", ""];
+		this.restaurant.priceLevel = 2;
+		this.restaurant.userScore = 3;
+		this.restaurant.allergyTags = ["Sipuliton", "Munaton"];
+		this.restaurant.openingHours = {monFri: 08002100, sat: 09002100, sun: 09001800};
+		this.restaurant.description = "Qwertyuiop. Asdfghjkl.<br>Zxcvbnm.";
+		
 	}
+	
 	render() {
 		<div id="restaurant">
 		<h2>{this.restaurant.name()}</h2><br>
-		<div id="restaurantPictures"><img src={this.restaurant.picture()} alt="Restaurant picture"></div>
-		<div id="restaurantStats"></div><br>
-		<div id="restaurantDesc"></div><br>
+		<div id="restaurantPictures"><img src={this.restaurant.pictures[0]()} alt="Restaurant picture"></div>
+		<div id="restaurantStats">Hintataso: {this.restaurant.priceLevel()}<br>
+		Käyttäjien arvio: {this.restaurant.userScore()}<br>
+		Allergiatunnisteet:<br>
+		{this.restaurant.allergyTags()}</div><br>
+		<div id="restaurantDesc">{this.restaurant.description()}</div><br>
 		<Input type="button" value="Lisää arvostelu" className="btn btn-primary mb-2">Lisää arvostelu></Input>
 		</div>
 	}
