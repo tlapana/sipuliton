ee3ed59b835fc68508d44ae8d8f94f184b361471
anthau <uusi2@localhost.localdomain>
2018-11-07 13:50:15 +0200

    aloitettu arkkitehtuurimuutokset

--- client/src/modules/home/index.js
+++
@@ -1,5 +0,0 @@
-// "Public" API for login 
-import Home from './components/Home';
-// other imports here
-
-export default { Home };
--- client/src/modules/login/index.js
+++
@@ -1,6 +0,0 @@
-// "Public" API for login 
-import Login from './components/Login';
-// other imports here
-// import reducer from './reducer';
-
-export default { Login, /* ... */ };
