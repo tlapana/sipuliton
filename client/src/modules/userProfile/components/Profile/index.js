97da05e094d3222859725591e6774290e5eaa06f
anthau <uusi2@localhost.localdomain>
2018-11-08 21:37:17 +0200

    update dev branch in profile

--- client/src/modules/userProfile/components/Profile/index.js
+++ client/src/modules/userProfile/components/Profile/index.js
@@ -1,7 +1,7 @@
 import React, { } from 'react';
 import { Button, Container, Row, Col } from 'reactstrap';
 
-
+ 
 class Profile extends React.Component {
       constructor(props) {
             super(props);
