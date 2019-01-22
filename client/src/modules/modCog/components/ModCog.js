5acf14eb6d397d5eabb57acd1342caabb0cd24e8
anthau <antto.hautamaki@elisanet.fi>
2019-01-22 23:09:34 +0200

    added UI to change password(not entire working)

--- client/src/modules/modCog/components/ModCog.js
+++ client/src/modules/modCog/components/ModCog.js
@@ -5,44 +5,66 @@
 */
 
 import React from 'react';
-import ReactStars from 'react-stars'
-import { Button } from 'reactstrap';
-import Select from 'react-select';
-import { Auth } from 'aws-amplify';
-import { stringify } from 'querystring';
+import { Link, Redirect } from 'react-router-dom';
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+} from 'reactstrap';
+import { Auth } from "aws-amplify";
+import commonComponents from '../../common';
+
+import * as validationUtil from "../../../validationUtil";
+import LocalizedStrings from 'react-localization';
 
 
  class ModCog extends React.Component {
 
+  //lborrowed directly from Search
+
   constructor(props) {
     super(props);
+  this.state = {
+      isLoading: false,
+      isValid:false,
+      retypePass:'',
+      isValid:''
 
-    this.state = {
+};
+this.onPasswordChanged = this.onPasswordChanged.bind(this);
 
-      text:'222'
-    }
-
-// Collect confirmation code and new password, then
-/* Auth.forgotPasswordSubmit('Antha1', "962110", '12345678')
-    .then(data => alert(data))
-    .catch(err => alert('2' + JSON.stringify(err)));
-  
-  } */
+    
 }
-getAuthCode()  {
 
-   alert('ok1')
+getAuthCode(e)  {
+
+   alert('Password legal='+ this.state.isValid)
+
+   
    Auth.currentAuthenticatedUser()
-   .then(user => {
-
-
-    alert(completeNewPassword(user , 'aaa',null));
-   })
+   .then(user =>Auth.changePassword(user,'testuser1','Testuser13') )
+    .catch(err => alert('2' + JSON.stringify(err)));
+   ;
    
 
    
 
 }
+
+onPasswordChanged(e) {
+  const password = e.target.value;
+  const reLowerCase = /[a-z]/;
+  const reNumber = /[0-9]/;
+  
+  const isValidP = validationUtil.validatePassword(password);
+
+  this.setState({isValid: isValidP})
+
+
+
+}
+
   //Actios to be caried upon mounting
   componentDidMount() {
 
@@ -50,18 +72,35 @@ getAuthCode()  {
  
   
   render() {
-    
+    const { VInput, } = commonComponents;
     return(
       <div>
-     <button onClick={()=>{this.getAuthCode()}}>
-      Lähetä aktivointilinkki</button><br/>
 
-    <form><em>New Password </em>
-    <input type="text"/><br/>
-    <em>Confirm password</em><input type="text"/><br/>
-    <input type="Submit" value="Vaihda salasanaa"/>
+
+    <form>
+      <em>Nykyinen salasana </em>
+    <FormGroup>
+    {this.state.error}
+    <VInput type="password"   name="password" onChange={this.onPasswordChanged} errormsg={'error'} isValid={this.state.passwordValid} value={this.state.password} onChange={this.onPasswordChanged}/>
+</FormGroup>
+
+<em>New Password </em>
+    <FormGroup>
+    {this.state.error}
+    <VInput type="password"   name="password" onChange={this.onPasswordChanged} errormsg={'error'} isValid={this.state.passwordValid} value={this.state.password} onChange={this.onPasswordChanged}/>
+</FormGroup>
+
+<em>Retype new password </em>
+    <FormGroup>
+    {this.state.error}
+    <VInput type="password"   name="password" onChange={this.onPasswordChanged} errormsg={'error'} isValid={this.state.passwordValid} value={this.state.password} onChange={this.onPasswordChanged}/>
+</FormGroup>
+<button onClick={()=>{this.getAuthCode()}}>
+       changePassword </button><br/>
+
+   
     
-    </form>) </div>);
+    </form> </div>);
     
     
   }
