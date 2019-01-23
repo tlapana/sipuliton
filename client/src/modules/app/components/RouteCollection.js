5456def45c99f5ada862b2ac0608f795cd9ee99a
Tommi Mansikka <mansikk6@student.tut.fi>
2019-01-23 16:57:32 +0200

    Removed bunch of duplicate or unsused imports

--- client/src/modules/app/components/RouteCollection.js
+++ client/src/modules/app/components/RouteCollection.js
@@ -6,12 +6,10 @@ import login from '../../login';
 import register from '../../register';
 import forgotPassword from '../../forgotpassword';
 import profile from '../../profile';
-import Review  from '../../MyReview/components/MyReview';
-import myReviewEdit1  from '../../MyReview/components/myReviewsEdit';
 import about from '../../about';
 import map from '../../map'
-import Review from '../../myReviews'
-import myReviewEdit1 from '../../myReviews/components/Review/myReviewsEdit'
+import myReviews from '../../myReviews'
+import myReviewEdit from '../../myReviews/components/Review/myReviewsEdit'
 import restaurant from '../../restaurant';
 import NotFound from './NotFound';
 
@@ -40,14 +38,10 @@ class RouteCollection extends React.Component {
         <Route path="/:language/profile/" component={Profile} />
         <Route path="/:language/map/:searchParameters" component={Map} />
         <Route path="/:language/map" component={Map} />
-<<<<<<< HEAD
-        <Route path="/:language/myReview" component={Review} />
-        <Route path="/:language/myReviewEdit" component={myReviewEdit1 } />
 
-=======
-        <Route path="/:language/myReviews" component={Review} />
-        <Route path="/:language/myReviewEdit" component={myReviewEdit1} />
->>>>>>> 7ad5105a9a93bcf16c5f27ba44f8197f7e7aaf4e
+        <Route path="/:language/myReviews" component={myReviews} />
+        <Route path="/:language/myReviewEdit" component={myReviewEdit} />
+        
         <Route path="/:language/restaurant/:id" component={Restaurant} />
         <Route path="/:language/about" component={About} />
         <Route component={NotFound} />
