d682ccdcfb8df9297e6f0903a81b7a45607ed5a1
System Administrator <root@Viljami-MacBook-Pro.local>
2019-01-24 20:42:40 +0200

    Fixed profile page styles and added placeholder image if image url is empty

--- client/src/modules/app/components/RouteCollection.js
+++ client/src/modules/app/components/RouteCollection.js
@@ -31,12 +31,12 @@ class RouteCollection extends React.Component {
         <Route path="/:language/login/" component={Login} />
         <Route path="/:language/register/" component={Register} />
         <Route path="/:language/forgot-password/" component={ForgotPassword} />
-        <Route path="/:language/profile/:id" component={Profile} />
+        <Route path="/:language/profile/" component={Profile} />
         <Route path="/:language/map/:searchParameters" component={Map} />
         <Route path="/:language/map" component={Map} />
         <Route path="/:language/myReview" component={Review} />
         <Route path="/:language/myReviewEdit" component={myReviewEdit1 } />
-        <Route path="/:language/restaurant/" component={Restaurant} />
+        <Route path="/:language/restaurant/:id" component={Restaurant} />
         <Route path="/:language/about" component={About} />
         <Route component={NotFound} />
       </Switch>
