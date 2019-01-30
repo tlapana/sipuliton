d89689b96e29296c30023bbd37d08b8bb0f960d6
Sergei Häyrynen <sergei.hayrynen@veracell.eu>
2019-01-30 16:40:06 +0200

    Modify Restaurant lambda for deployment to AWS

--- services/lambda/sipuliton-backend/restaurant/restaurant.js
+++ services/lambda/sipuliton-backend/restaurant/restaurant.js
@@ -1,6 +1,16 @@
 
 let response;
 
+const pg = require('pg');
+const AWS = require('aws-sdk');
+
+// Database credentials
+const region = 'eu-central-1';
+const dbPort = 5432;
+const dbUsername = 'lambda_user'; 
+const dbName = 'sipuliton'; 
+const dbEndpoint = 'sipulitondb.c15ehja7hync.eu-central-1.rds.amazonaws.com';
+
 /**
  *
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
@@ -35,53 +45,68 @@ let response;
  * @returns {Object} object.body - JSON Payload to be returned
  * 
  */
-exports.lambdaHandler = async (event, context) => {
+exports.lambdaHandler = async (event, context, cb) => {
     try {
-        const restaurantId = event.queryStringParameters.restaurantId
+        var signer = new AWS.RDS.Signer();
+        signer.getAuthToken({ // uses the IAM role access keys to create an authentication token
+          region: region,
+          hostname: dbEndpoint,
+          port: dbPort,
+          username: dbUsername
+        }, function(err, token) {
+            if (err) {
+                console.log(`could not get auth token: ${err}`);
+                throw(err);
+            } else {
+                const restaurantId = event.queryStringParameters.restaurantId
 
-        var collectRestaurantPage = `
-        SELECT restaurant.restaurant_id as restaurant_id, name, email, website, street_address, geo_location, 
-               rating_overall,
-               rating_reliability,
-               rating_variety,
-               rating_service_and_quality 
-        FROM restaurant INNER JOIN restaurant_diet_stats
-        ON restaurant.restaurant_id=restaurant_diet_stats.restaurant_id
-        WHERE restaurant.restaurant_id = $1
-        `;
+                var collectRestaurantPage = `
+                SELECT restaurant.restaurant_id as restaurant_id, name, email, website, street_address, geo_location, 
+                    rating_overall,
+                    rating_reliability,
+                    rating_variety,
+                    rating_service_and_quality 
+                FROM restaurant LEFT JOIN restaurant_diet_stats
+                ON restaurant.restaurant_id=restaurant_diet_stats.restaurant_id
+                WHERE restaurant.restaurant_id = $1
+                `;
 
-        var pg = require("pg");
-
-        //TODO: Before deploying, change to a method for fetching Amazon RDS credentials
-        var conn = "postgres://sipuliton:sipuliton@sipuliton_postgres_1/sipuliton";
-        const client = new pg.Client(conn);
-        await client.connect((err) => {
-                console.log("Connecting")
-                if (err){
-                    console.error("Failed to connect client")
-                    console.error(err)
-                    throw err
+                var client = new pg.Client({
+                    host: dbEndpoint,
+                    port: 5432,
+                    user: dbUsername,
+                    password: token,
+                    database: dbName,
+                    ssl: 'Amazon RDS'
+                  });
+                client.connect()
+                
+                client.query(collectRestaurantPage, [restaurantId], function(err, resRestaurant, fields) {
+                    client.end()
+                    if (err) {
+                        console.log(`could not execute query: ${err}`);
+                        throw(err);
+                      } else {
+                        var jsonString = JSON.stringify(resRestaurant.rows);
+                        var jsonObjRestaurant = JSON.parse(jsonString);
+                        var bodyJSON = {
+                            'restaurant': jsonObjRestaurant
+                        }
+                    
+                    response = {
+                        'statusCode': 200,
+                        //TODO: Handle CORS in AWS api gateway settings prior to deployment
+                        'headers': {
+                            'Access-Control-Allow-Origin': '*'
+                        },
+                        'body': JSON.stringify(bodyJSON)
+                    };
+                    cb(undefined, response)
                 }
-        });
-        
-        const resRestaurant = await client.query(collectRestaurantPage, [restaurantId]);
-        var jsonString = JSON.stringify(resRestaurant.rows);
-        var jsonObjRestaurant = JSON.parse(jsonString);
-        await client.end()
 
-        var bodyJSON = {
-            'restaurant': jsonObjRestaurant
+            });
         }
-
-        response = {
-            'statusCode': 200,
-            //TODO: Handle CORS in AWS api gateway settings prior to deployment
-            'headers': {
-                'Access-Control-Allow-Origin': '*'
-            },
-            'body': JSON.stringify(bodyJSON)
-        };
-
+        });
 
     } catch (err) {
         console.error(err);
