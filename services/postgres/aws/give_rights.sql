dae4c2266acb01adec7fd31282c9982232d78e40
Sergei Häyrynen <sergei.hayrynen@veracell.eu>
2019-01-30 19:46:31 +0200

    Add necessary rights for lambda_user

--- services/postgres/aws/give_rights.sql
+++ services/postgres/aws/give_rights.sql
@@ -1,6 +1,8 @@
 GRANT SELECT,UPDATE ON ALL TABLES IN SCHEMA public to lambda_user;
+GRANT INSERT ON diet_vote TO lambda_user;
 GRANT DELETE ON review TO lambda_user;
 GRANT DELETE ON review_reject_log TO lambda_user;
 GRANT DELETE ON review_from TO lambda_user;
 GRANT DELETE ON review_accept_log TO lambda_user;
 GRANT DELETE ON thumbs TO lambda_user;
+GRANT DELETE ON review_diet TO lambda_user;
